1add123c7f111fb145eb876a5f8bdd131cd2faac
NunoLealF <nunoleal21@protonmail.ch>
2023-07-27 13:51:27 +0100

    New PC, just testing to see if this works

--- Boot/Legacy/Memory/A20.c
+++ Boot/Legacy/Memory/A20.c
@@ -5,3 +5,6 @@
 #include "../Stdint.h"
 
 // To-do: Make functions related to A20.
+
+// You probably want to use the Fast A20 method (supported on PCs since PS/2)
+// But first, check if it's already enabled
