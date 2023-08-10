e912fe57506af27edbb036c119d94689582a4c17
NunoLealF <nunoleal21@protonmail.ch>
2023-08-10 17:19:51 +0100

    Finally able to switch back from real mode!!

--- Boot/Legacy/Rm/Rm.asm
+++ Boot/Legacy/Rm/Rm.asm
@@ -2,48 +2,267 @@
 ; This file is part of the Serra project, which is released under the MIT license.
 ; For more information, please refer to the accompanying license agreement. <3
 
-[ORG 0D000h] ; We are at 7C00h.
-[BITS 16] ; This is 16-bit code.
+[ORG 0C000h] ; We are at C000h.
+[BITS 32] ; This is 32-bit code.. for now.
 
-; Just in case
+; Start of our program pretty much
 
-cli
+; - [Preparations]
+; - Prepare a 16-bit real mode 'payload' (ideally at 0D00-0F00h)
+; - Prepare a suitable 16-bit real mode GDT and IDT
+; - Prepare a suitable 32-bit protected mode GDT
+; - Prepare some sort of data area to tell the payload what to do
 
-; Reset segment registers to 0 (for some reason real mode's like that)
+; - [Process]
+; - https://xem.github.io/minix86/manual/intel-x86-and-64-manual-vol3/o_fe12b1e2a880e0ce-326.html
+; - (Essentially, disable interrupts, go into 16 bit protected mode, switch from protected to real
+; - mode, execute your payload, and do the reverse of that)
 
-mov ax, 0
-mov ds, ax
-mov es, ax
-mov fs, ax
-mov gs, ax
-mov ss, ax
+; ...
+; ...
 
-mov sp, 7C00h
-; we really need to find somewhere where we can save the stack pointer/segment
-; like, push and pop doesn't work because it relies on the stack lol
+prepareRealMode32:
 
-; (btw, if this doesn't work - a possible candidate is that the GDT might be wrong)
-; (like, it might be growing downwards instead of upwards, that sorta thing)
+  ; Disable interrupts (except NMIs); as we're switching between different modes, keeping them
+  ; on *will* interfere with the process and potentially crash the system.
 
-; Okay, test
+  cli
 
-sti
-mov ah, 0
-mov al, 13h
-int 10h
+  ; Save ESP (stack pointer)
 
-mov ah, 0Ch
-mov al, 0Bh
-mov bh, 0
-mov cx, 0
-mov dx, 0
-int 10h
+  mov [saveStack], esp
 
-loopuwu:
+  ; Load 16-bit GDT
 
-cli
-hlt
-jmp loopuwu
+  lgdt [realModeGdtDescriptor]
+
+  ; Far jump to 16-bit protected mode (setting cs to our code selector, 08h)
+
+  jmp 08h:prepareRealMode16
+
+
+; ...
+; ...
+
+[BITS 16]
+
+prepareRealMode16:
+
+  ; Load ds-ss with our new GDT's data segment.
+
+  mov ax, 10h
+  mov ds, ax
+  mov es, ax
+  mov fs, ax
+  mov gs, ax
+  mov ss, ax
+
+  ; Load real mode IDT
+
+  lidt [realModeIdtDescriptor]
+
+  ; Disable protected mode by clearing the first bit of CR0.
+
+  mov eax, cr0
+  and eax, 0FFFFFFFEh
+  mov cr0, eax
+
+  ; Far jump to our real mode code
+
+  jmp 00h:initRealMode
+
+
+; ...
+; ...
+
+initRealMode:
+
+  ; Just in case
+
+  cli
+
+  ; Reset segment registers to 0 (for some reason real mode's like that)
+
+  mov ax, 0
+  mov ds, ax
+  mov es, ax
+  mov fs, ax
+  mov gs, ax
+  mov ss, ax
+
+  ; Set stack to 7C00h
+
+  mov sp, 7C00h
+
+
+; ...
+; ...
+
+setupRealMode:
+
+  ; Test (this should display a <3 on the screen)
+
+  sti
+
+  mov ah, 0Eh
+  mov al, '<'
+  mov bh, 0
+  mov bl, 07h
+
+  int 10h
+
+  mov al, '3'
+  int 10h
+
+  jmp prepareProtectedMode16
+
+; ...
+; ...
+
+prepareProtectedMode16:
+
+  ; Disable interrupts
+
+  cli
+
+  ; Load protected mode GDT
+
+  lgdt [protectedModeGdtDescriptor]
+
+  ; Enable protected mode
+
+  mov eax, cr0
+  or eax, 1
+  mov cr0, eax
+
+  ; Far jump
+
+  jmp 08h:prepareProtectedMode32
+
+
+; ...
+; ...
+
+[BITS 32]
+
+prepareProtectedMode32:
+
+  ; We're now in protected mode
+  ; Restore segment vars
+
+  mov eax, 10h
+  mov ds, eax
+  mov es, eax
+  mov fs, eax
+  mov gs, eax
+  mov ss, eax
+
+  ; Restore ESP
+
+  mov esp, [saveStack]
+
+  ; Return (because of the C calling convention, everything else should be saved)
+
+  ret
+
+
+; -----------------------------------
+
+; This is where we'll reserve our stack pointer - although we save the state of every other
+; register with the C calling conventions, we'll still need to know the stack pointer so we can
+; actually pop those registers from the stack.
+
+saveStack: dd 0
+
+
+; -----------------------------------
+
+; 16-bit real and protected mode GDT descriptor.
+
+realModeGdtDescriptor:
+
+  dw 24 - 1 ; We have three segments, each of which is 8 bytes long, so our GDT is 24 bytes.
+  dd realModeGdt ; The location of our GDT is at the realModeGdt label.
+
+; 16-bit real and protected mode GDT.
+
+realModeGdt:
+
+  ; Segment 0 (Null segment)
+
+  dw 0000h ; Limit (bits 0-15)
+  dw 0000h ; Base (bits 0-15)
+  db 00h ; Base (bits 16-23)
+  db 00000000b ; Access byte (bits 0-7)
+  db 00000000b ; Limit (bits 16-19) and flags (bits 0-7)
+  db 00h ; Base (bits 24-31)
+
+  ; Segment 1 (Code segment, spans all 1MiB of memory)
+
+  dw 0FFFFh ; Limit (bits 0-15)
+  dw 0000h ; Base (bits 0-15)
+  db 00h ; Base (bits 16-23)
+  db 10011010b ; Access byte (bits 0-7)
+  db 00000000b ; Limit (bits 16-19) and flags (bits 0-7)
+  db 00h ; Base (bits 24-31)
+
+  ; Segment 2 (Data segment, spans all 1MiB of memory)
+
+  dw 0FFFFh ; Limit (bits 0-15)
+  dw 0000h ; Base (bits 0-15)
+  db 00h ; Base (bits 16-23)
+  db 10010010b ; Access byte (bits 0-7)
+  db 00000000b ; Limit (bits 16-19) and flags (bits 0-7)
+  db 00h ; Base (bits 24-31)
+
+; 16-bit real mode IDT descriptor.
+
+realModeIdtDescriptor:
+
+  dw (400h - 1) ; The real-mode IDT is pretty much always 400h bytes long.
+  dd 0 ; It's also usually at the very start of memory (0h).
+
+
+; -----------------------------------
+
+; 32-bit protected mode GDT descriptor.
+
+protectedModeGdtDescriptor:
+
+  dw 24 - 1 ; Three eight-byte segments, minus one byte.
+  dd protectedModeGdt ; The location of our GDT is the protectedModeGdt label.
+
+; 32-bit protected mode GDT.
+
+protectedModeGdt:
+
+  ; Segment 0 (Null segment)
+
+  dw 0000h ; Limit (bits 0-15)
+  dw 0000h ; Base (bits 0-15)
+  db 00h ; Base (bits 16-23)
+  db 00000000b ; Access byte (bits 0-7)
+  db 00000000b ; Limit (bits 16-19) and flags (bits 0-7)
+  db 00h ; Base (bits 24-31)
+
+  ; Segment 1 (Code segment, spans all 4GiB of memory)
+
+  dw 0FFFFh ; Limit (bits 0-15)
+  dw 0000h ; Base (bits 0-15)
+  db 00h ; Base (bits 16-23)
+  db 10011010b ; Access byte (bits 0-7)
+  db 11001111b ; Limit (bits 16-19) and flags (bits 0-7)
+  db 00h ; Base (bits 24-31)
+
+  ; Segment 2 (Data segment, spans all 4GiB of memory)
+
+  dw 0FFFFh ; Limit (bits 0-15)
+  dw 0000h ; Base (bits 0-15)
+  db 00h ; Base (bits 16-23)
+  db 10010010b ; Access byte (bits 0-7)
+  db 11001111b ; Limit (bits 16-19) and flags (bits 0-7)
+  db 00h ; Base (bits 24-31)
+
 
 ; -----------------------------------
 
--- Boot/Legacy/Rm/RmWrapper.c
+++ Boot/Legacy/Rm/RmWrapper.c
@@ -12,3 +12,33 @@ void realMode(void) {
   __asm__("movl $0xC000, %%eax; call *%%eax" : : : "eax");
 
 }
+
+// C000-C800: Protected -> real mode code
+// C800-D000: Real -> protected mode code
+// D000-DE00: Real mode payload
+// DE00-E000: Data
+
+// (This is supposed to be stored and then later on read by the real-mode code btw.)
+// (Probably should allocate it at like, DF00 tbh)
+// You want a function that 'initializes' this I think, DF00 would be the best area imo.
+
+// Right now, this is 36 bytes long
+// Also, it's a typedef, so you have to declare like a new struct at DF00
+
+typedef struct {
+
+  uint32 Eax;
+  uint32 Ebx;
+  uint32 Ecx;
+  uint32 Edx;
+
+  uint32 Esi;
+  uint32 Edi;
+  uint32 Ebp;
+
+  uint16 Ds;
+  uint16 Es;
+  uint16 Fs;
+  uint16 Gs;
+
+} __attribute__((packed)) realModeRegisters;
