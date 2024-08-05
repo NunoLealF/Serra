6db6a7ef997ca7993acb20e46b18d7d19943bccb
NunoLealF <nunoleal21@protonmail.ch>
2024-08-05 09:44:46 +0100

    Add Cpu/ folder

---
+++ Boot/Legacy/Core/Cpu/Cpu.c
@@ -0,0 +1,7 @@
+// Copyright (C) 2024 NunoLealF
+// This file is part of the Serra project, which is released under the MIT license.
+// For more information, please refer to the accompanying license agreement. <3
+
+#include "../../Shared/Stdint.h"
+
+// (Todo: functions)
---
+++ Boot/Legacy/Core/Cpu/Cpu.h
@@ -0,0 +1,11 @@
+// Copyright (C) 2024 NunoLealF
+// This file is part of the Serra project, which is released under the MIT license.
+// For more information, please refer to the accompanying license agreement. <3
+
+#ifndef SERRA_CPU_H
+#define SERRA_CPU_H
+
+  // (Todo: functions from Cpu.c)
+
+
+#endif
