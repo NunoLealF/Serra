74d59fd9c73e2c7d11818a2ecdd1280c5516abf8
NunoLealF <nunoleal21@protonmail.ch>
2025-05-25 18:30:26 +0100

    Bezier curve demo

--- Common/Kernel/Core.c
+++ Common/Kernel/Core.c
@@ -24,11 +24,33 @@ void KernelCore(commonInfoTable* InfoTable) {
 
       Memset((void*)Buffer, (uint8)(Y * 255 / InfoTable->Display.Graphics.LimitY), Size);
 
-      if (Y < 100) {
-        Memcpy((void*)Buffer, (const void*)(&InfoTable + Y*16), Size);
-      } else {
-        Memset((void*)Buffer, (uint8)(Y * 255 / InfoTable->Display.Graphics.LimitY), Size);
-      }
+    }
+
+    // (Draw a bezier curve)
+
+    uint16 Width = 480;
+    uint16 Height = 480;
+
+    uint16 WidthOffset = (InfoTable->Display.Graphics.LimitX - Width) / 2;
+    uint16 HeightOffset = (InfoTable->Display.Graphics.LimitY - Height) / 2;
+
+    double Points[3][2] = {{0, 0.5}, {0, 0}, {0.5, 0}};
+
+    for (auto j = 0; j < 1024; j++) {
+
+      uint16 Bezier[2];
+
+      double a = j / 1024.f;
+      double b = (1024-j) / 1024.f;
+
+      Bezier[0] = (uint16)((double)Width * ((a * a * Points[0][0]) + (2 * a * b * Points[1][0]) + (b * b * Points[2][0]))) + WidthOffset;
+      Bezier[1] = (uint16)((double)Height * ((a * a * Points[0][1]) + (2 * a * b * Points[1][1]) + (b * b * Points[2][1]))) + HeightOffset;
+
+      uint64 Addr = InfoTable->Display.Graphics.Framebuffer.Address;
+      Addr += (Bezier[1] * InfoTable->Display.Graphics.Pitch);
+      Addr += (Bezier[0] * InfoTable->Display.Graphics.Bits.PerPixel / 8);
+
+      Memset((void*)Addr, 0xFF, (InfoTable->Display.Graphics.Bits.PerPixel / 8));
 
     }
 
