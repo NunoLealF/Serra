4389a78c9ee7c23bf2815919f7863fb9020dd162
NunoLealF <nunoleal21@protonmail.ch>
2025-05-27 20:06:12 +0100

    Fix comments in String.h

--- Common/Kernel/Libraries/String.h
+++ Common/Kernel/Libraries/String.h
@@ -10,15 +10,14 @@
   #include "Stdint.h"
   #include "../Constructors/System/System.h"
 
-  // Include functions provided by Memory/Memcpy.c (TODO)
+  // Include functions provided by Memory/Memory.c (TODO)
 
   void Memcpy(void* Destination, const void* Source, uint64 Size);
   void* memcpy(void* Destination, const void* Source, uint64 Size);
 
-  // Include functions provided by Memory/Memset.c (TODO)
-
-  void MemsetBlock(void* Buffer, const void* Block, uint64 Size, uint64 BlockSize);
   void Memset(void* Buffer, uint8 Character, uint64 Size);
   void* memset(void* Buffer, uint8 Character, uint64 Size);
 
+  void MemsetBlock(void* Buffer, const void* Block, uint64 Size, uint64 BlockSize);
+  
 #endif
